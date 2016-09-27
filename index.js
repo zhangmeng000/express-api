850e72bc319cbdfe7f3bed3cf8b3c86f6dcecebd
zhangmeng000 <1550685677@qq.com>
2016-09-27 16:09:22 +0800

    mm

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>')
+app.get('/:name', function (req,res) {
+	var username = req.params.name;
+	var page = "<html>" + "<body>" + "<h2>" + username + "的购物车" + "</h2>" + "</body>" + "</html>"
+  res.send(page)
 })
 app.get('/aaa', function (req, res) {
+	var page = "<html>" + "<body>" + "<h2>aaa.html</h2>" + "</body>" + "</html>"
   console.log('hello aaa')
 })
 app.listen(3000,function(){
